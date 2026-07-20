2470f946c6aee050ebe12c14fff11622b1057299
Scott <sschus90@hotmail.com>
2026-07-20 12:56:18 -0500

    update nh version add method for postgres

--- NHibernate.Core.Fluent/NHibernateSessionManager.cs
+++ NHibernate.Core.Fluent/NHibernateSessionManager.cs
@@ -8,18 +8,23 @@ namespace NHibernate.Core.Fluent
 {
     public class NHibernateSessionManager : NHibernateSessionManagerBase
     {
-        private static FluentConfiguration GetFluentConfiguration<TDialect, TDriver>(Assembly mappingAssembly, Action<Configuration> cfg = null, string connectionString = null)
+        private static FluentConfiguration GetFluentConfiguration<TDialect, TDriver>(Assembly mappingAssembly, Action<Configuration>? cfg = null, string? connectionString = null)
             where TDialect : Dialect.Dialect where TDriver : IDriver
         {
             var config = GetConfiguration<TDialect, TDriver>(cfg, connectionString);
             return Fluently.Configure(config).Mappings(m => m.FluentMappings.AddFromAssembly(mappingAssembly));
         }
-        public static ISessionFactory ConfigureFluently(Assembly mappingAssembly, Action<Configuration> cfg = null, string connectionString = null)
+        public static ISessionFactory ConfigureFluently(Assembly mappingAssembly, Action<Configuration>? cfg = null, string? connectionString = null)
         {
             SessionFactory = GetFluentConfiguration<MySQL55Dialect, MySqlDataDriver>(mappingAssembly, cfg, connectionString).BuildSessionFactory();
             return SessionFactory;
         }
-        public static ISessionFactory ConfigureFluently<TDialect, TDriver>(Assembly mappingAssembly, Action<Configuration> cfg = null, string connectionString = null)
+        public static ISessionFactory ConfigureFluentlyPostgres(Assembly mappingAssembly, Action<Configuration>? cfg = null, string? connectionString = null)
+        {
+            SessionFactory = GetFluentConfiguration<PostgreSQLDialect, NpgsqlDriver>(mappingAssembly, cfg).BuildSessionFactory();
+            return SessionFactory;
+        }
+        public static ISessionFactory ConfigureFluently<TDialect, TDriver>(Assembly mappingAssembly, Action<Configuration>? cfg = null, string? connectionString = null)
             where TDialect : Dialect.Dialect where TDriver : IDriver
         {
             SessionFactory = GetFluentConfiguration<TDialect, TDriver>(mappingAssembly, cfg, connectionString).BuildSessionFactory();
